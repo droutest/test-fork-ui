e544587846d36e98af15351e542c28e0dd87dbe6
DR <342252328@qq.com>
2016-02-22 10:40:33 +0800

    aaa

---
+++ src/main/java/com/mook/test_fork_ui/App.java
@@ -0,0 +1,12 @@
+package com.mook.test_fork_ui;
+
+/**
+ * Hello world!
+ * @author DR
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
